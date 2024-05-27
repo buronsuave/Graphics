202efd03f2be7f0c24c491ce607a868ba1921d7a
buronsuave <davidlopeztorres03@gmail.com>
2024-05-27 00:05:14 -0600

    Minor keyboard changes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "flyeye.h"
 
 void drawFlyEye(sf::RenderWindow* window, FlyEye flyeye);
-void drawVertex(sf::RenderWindow* window, std::pair<double, double> coords, sf::Color color);
+// void drawVertex(sf::RenderWindow* window, std::pair<double, double> coords, sf::Color color);
 void drawEdge(sf::RenderWindow* window, std::pair<std::pair<double, double>, std::pair<double, double>> pair_coords, sf::Color color);
 
 int main()
@@ -75,44 +75,44 @@ int main()
                 }
 
                 // Color 1 (white)
-                else if (event.key.code == sf::Keyboard::Numpad1)
+                else if (event.key.code == sf::Keyboard::Numpad1 || event.key.code == sf::Keyboard::Num1)
                 {
                     flyeye.setC(sf::Color::White);
                 }
 
                 // Color 2 (green)
-                else if (event.key.code == sf::Keyboard::Numpad2)
+                else if (event.key.code == sf::Keyboard::Numpad2 || event.key.code == sf::Keyboard::Num2)
                 {
                     flyeye.setC(sf::Color::Green);
                 }
 
                 // Color 3 (red)
-                else if (event.key.code == sf::Keyboard::Numpad3)
+                else if (event.key.code == sf::Keyboard::Numpad3 || event.key.code == sf::Keyboard::Num3)
                 {
                     flyeye.setC(sf::Color::Red);
                 }
 
                 // Color 4 (blue)
-                else if (event.key.code == sf::Keyboard::Numpad4)
+                else if (event.key.code == sf::Keyboard::Numpad4 || event.key.code == sf::Keyboard::Num4)
                 {
                     flyeye.setC(sf::Color::Blue);
                 }
 
                 // Color 5 (yellow)
-                else if (event.key.code == sf::Keyboard::Numpad5)
+                else if (event.key.code == sf::Keyboard::Numpad5 || event.key.code == sf::Keyboard::Num5)
                 {
                     flyeye.setC(sf::Color::Yellow);
                 }
 
                 // Add vertex
-                else if (event.key.code == sf::Keyboard::Add)
+                else if (event.key.code == sf::Keyboard::Add || event.key.code == sf::Keyboard::M)
                 {
                     if (flyeye.getV() != 30)
                         flyeye.setV(flyeye.getV() + 1);
                 }
 
                 // Remove vertex
-                else if (event.key.code == sf::Keyboard::Subtract)
+                else if (event.key.code == sf::Keyboard::Subtract || event.key.code == sf::Keyboard::N)
                 {
                     if (flyeye.getV() != 5)
                         flyeye.setV(flyeye.getV() - 1);
@@ -143,13 +143,13 @@ void drawFlyEye(sf::RenderWindow* window, FlyEye flyeye)
     std::vector<std::pair<double, double>> vertices_coords;
     for (int i = 0; i < v; ++i)
     {
-        std::pair<double, double> vertex_coords(r*cos(i*t_in + t) + o.first, r*sin(i*t_in + t) + o.second);
+        std::pair<double, double> vertex_coords(r*cos(i*t_in + t) + o.first, -r*sin(i*t_in + t) + o.second);
         vertices_coords.push_back(vertex_coords);
     }
 
     // Draw vertices
-    for (auto vertex_coords:vertices_coords)
-        drawVertex(window, vertex_coords, c);
+    // for (auto vertex_coords:vertices_coords)
+    //     drawVertex(window, vertex_coords, c);
 
     // Draw edges
     for (auto edge:e)
@@ -161,14 +161,14 @@ void drawFlyEye(sf::RenderWindow* window, FlyEye flyeye)
     }
 }
 
-void drawVertex(sf::RenderWindow* window, std::pair<double, double> coords, sf::Color color)
-{
-    // Draw circle (vertex)
-    sf::CircleShape c(5.f);
-    c.setFillColor(color);
-    c.setOrigin(coords.first, coords.second);
-    window->draw(c);
-}
+// void drawVertex(sf::RenderWindow* window, std::pair<double, double> coords, sf::Color color)
+// {
+//     // Draw circle (vertex)
+//     sf::CircleShape c(5.f);
+//     c.setFillColor(color);
+//     c.setOrigin(coords.first, coords.second);
+//     window->draw(c);
+// }
 
 void drawEdge(sf::RenderWindow* window, std::pair<std::pair<double, double>, std::pair<double, double>> pair_coords, sf::Color color)
 {
